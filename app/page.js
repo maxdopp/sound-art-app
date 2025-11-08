449d13f8970459e8051c2b59df7fb5ff8743fede
Jelee8 <josielee008@icloud.com>
2025-11-08 11:47:36 -0500

    added undo and redo functions

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 "use client";
 
 import P5Sketch from "@/app/components/P5Sketch";
-import { useState } from "react";
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const [brushColor, setBrushColor] = useState("#000000");
   const [brushSize, setBrushSize] = useState(10);
+  const p5InstanceRef = useRef(null);
+  
+  const undoStack = useRef([]);
+  const redoStack = useRef([]);
+  const drawing = useRef(false);
 
   const mySketch = (p) => {
     p.setup = () => {
@@ -52,12 +57,47 @@ export default function Home() {
     };
   }
 
+  //undo
+  const undo = () => {
+    if(!p5InstanceRef.current || undoStack.current.length === 0){
+      return;
+    }
+    const p = p5InstanceRef.current;
+    const lastState = undoStack.current.pop();
+    redoStack.current.push(p.canvas.toDataURL());
+    const prevState = undoStack.current[undoStack.current.length - 1];
+    const img = new Image();
+    img.src = prevState;
+    img.onload = () => {
+      p.clear();
+      p.image(img, 0, 0);
+    };
+  };
+
+  //redo
+  const redo = () => {
+    if(!p5InstanceRef.current || redoStack.current.length === 0){
+      return;
+    }
+    const p = p5InstanceRef.current;
+    const redoState = redoStack.current.pop();
+    undoStack.current.push(p.canvas.toDataURL());
+    const prevState = undoStack.current[undoStack.current.length - 1];
+    const img = new Image();
+    img.src = redoState;
+    img.onload = () => {
+      p.clear();
+      p.image(img, 0, 0);
+    };
+  };
+
   return (
     <main className="flex flex-row items-center justify-center min-h-screen">
       <Link href="/gallery" className="mt-4 underline text-blue-600">
         View Gallery →
       </Link>
       <P5Sketch sketch={bar} />
+
       <P5Sketch sketch={mySketch} />
     </main>
   );
