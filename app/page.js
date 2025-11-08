de6c81b39bdeb5337ac878e2d8523dfa92448888
Max Dopp <mdopp2005@gmail.com>
2025-11-07 23:26:14 -0500

    need to add old features

--- app/page.js
+++ app/page.js
@@ -1,28 +1,44 @@
 "use client";
 
 import P5Sketch from "@/components/P5Sketch";
+import { useState } from "react";
 
 export default function Home() {
+  const [brushColor, setBrushColor] = useState("#000000");
+  const [brushSize, setBrushSize] = useState(10);
+
   const mySketch = (p) => {
     let x = 0;
 
     p.setup = () => {
-      p.createCanvas(window.innerWidth, window.innerHeight);
+      p.createCanvas(window.innerWidth * (3/4), window.innerHeight);
       p.background(220);
     };
 
     p.draw = () => {
-      p.fill(0);
-      p.circle(p.mouseX, p.mouseY, 30);
+      p.fill(brushColor);
+      p.circle(p.pmouseX, p.pmouseY, brushSize);
     };
 
     p.windowResized = () => {
-        p.resizeCanvas(window.innerWidth, window.innerHeight);
-      };
+        p.resizeCanvas(window.innerWidth * (3/4), window.innerHeight);
+    };
   };
 
+  const bar = (p) => {
+    p.setup = () => {
+      p.createCanvas(window.innerWidth * (1/4), window.innerHeight);
+      p.background(0);
+    }
+
+    p.windowResized = () => {
+        p.resizeCanvas(window.innerWidth * (1/4), window.innerHeight);
+    };
+  }
+
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
+    <main className="flex flex-row items-center justify-center min-h-screen">
+      <P5Sketch sketch={bar} />
       <P5Sketch sketch={mySketch} />
     </main>
   );
